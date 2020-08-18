fd9278ff3a17a5a379797f7f0b50fa0a60e2bc8f
Marcela Malaver <marcela9409@gmail.com>
2020-08-18 18:12:42 -0500

    React Big Calendar

--- src/calendar/CalendarScreen.js
+++ src/calendar/CalendarScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment'
 import { NavBar } from "../ui/NavBar";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../styles.css';
+
+const localizer = momentLocalizer(moment);
+const events = [{
+    title: 'Reunion del jefe',
+    start: moment().toDate(),
+    end: moment().add(2,'hours').toDate(),
+    bgcolor: '#fafafafa'
+}]
 
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className='calendar-screen'>
       <NavBar />
+      <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' />
     </div>
   );
 };

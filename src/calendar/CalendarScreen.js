fdf128396721cde1fc542541c37af03949baa824
Marcela Malaver <marcela9409@gmail.com>
2020-08-18 18:27:16 -0500

    Personalizar el cuadro y disparar eventos del calendario

--- src/calendar/CalendarScreen.js
+++ src/calendar/CalendarScreen.js
@@ -1,23 +1,48 @@
 import React from "react";
-import {Calendar, momentLocalizer} from 'react-big-calendar';
-import moment from 'moment'
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
 import { NavBar } from "../ui/NavBar";
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import '../styles.css';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "../styles.css";
+import { messages } from "../helpers/calendar-messages-es";
+import "moment/locale/es";
+
+moment.locale("es");
 
 const localizer = momentLocalizer(moment);
-const events = [{
-    title: 'Reunion del jefe',
+const events = [
+  {
+    title: "Reunion del jefe",
     start: moment().toDate(),
-    end: moment().add(2,'hours').toDate(),
-    bgcolor: '#fafafafa'
-}]
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafafa",
+    notes: "Llevar el informe",
+  },
+];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367cf7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+    return { style };
+  };
   return (
-    <div className='calendar-screen'>
+    <div className="calendar-screen">
       <NavBar />
-      <Calendar localizer={localizer} events={events} startAccessor='start' endAccessor='end' />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+      />
     </div>
   );
 };

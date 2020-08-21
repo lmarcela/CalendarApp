fed8b22fc7759d9c3ac7d201b677f1d2d66c4931
Marcela Malaver <marcela9409@gmail.com>
2020-08-21 18:52:51 -0500

    Añadir un nuevo evento

--- src/reducers/calendarReducer.js
+++ src/reducers/calendarReducer.js
@@ -2,28 +2,30 @@ import { types } from "../types/types";
 import moment from "moment";
 
 const initialState = {
-    events : [{
-        title: "Reunion del jefe",
-        start: moment().toDate(),
-        end: moment().add(2, "hours").toDate(),
-        bgcolor: "#fafafafa",
-        notes: "Llevar el informe",
-        user: {
-          _id: "123",
-          name: "Marcela",
-        },
-      }],
-    activeEvent:null
-}
+  events: [
+    {
+      title: "Reunion del jefe",
+      start: moment().toDate(),
+      end: moment().add(2, "hours").toDate(),
+      bgcolor: "#fafafafa",
+      notes: "Llevar el informe",
+      user: {
+        _id: "123",
+        name: "Marcela",
+      },
+    },
+  ],
+  activeEvent: null,
+};
 
-export const calendarReducer = (state=initialState, action)=>{
-    switch (action.type) {
-        /* case types.eventAddNew:
-          return { ...state, modalOpen: true };  */
-        case types.eventSetActive:
-          return { ...state, activeEvent: action.payload };
-    
-        default:
-          return state;
-      }
-}
+export const calendarReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.eventAddNew:
+      return { ...state, events: [...state.events, action.payload] };
+    case types.eventSetActive:
+      return { ...state, activeEvent: action.payload };
+
+    default:
+      return state;
+  }
+};

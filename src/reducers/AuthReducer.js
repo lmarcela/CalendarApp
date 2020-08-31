6d6a143c7c3bba6cdcf28033715c1c25f757e9d8
Marcela Malaver <marcela9409@gmail.com>
2020-08-31 18:16:17 -0500

    AuthReducer

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,38 @@
+import { types } from "../types/types";
+
+const initialState = {
+  checking: true,
+  /* uid: null,
+  name: null */  
+};
+
+export const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    /* case types.eventAddNew:
+      return { ...state, events: [...state.events, action.payload] };
+
+    case types.eventSetActive:
+      return { ...state, activeEvent: action.payload };
+
+    case types.eventClearActiveEvent:
+      return { ...state, activeEvent: null };
+
+    case types.eventUpdated:
+      return {
+        ...state,
+        events: state.events.map((e) =>
+          e.id === action.payload.id ? action.payload : e
+        ),
+      };
+
+    case types.eventDeleted:
+      return {
+        ...state,
+        events: state.events.filter((e) => e.id !== state.activeEvent.id),
+        activeEvent: null
+      }; */
+
+    default:
+      return state;
+  }
+};
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import { uiReducer } from "./uiReducer";
 import { calendarReducer } from "./calendarReducer";
+import { authReducer } from "./AuthReducer";
 
 export const rootReducer = combineReducers({
   ui: uiReducer,
   calendar: calendarReducer,
-  /* auth: AuthReducer,
-   */
+  auth: authReducer,
 });
